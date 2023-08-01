e698ba5014c937692ad1cb265aa3221e7d7dbc89
ykkssyaa <mr.ender.03@mail.ru>
2023-08-01 14:30:43 +0300

    update postgres version in docker-compose.yml, add logger in db init

--- package/common/db.go
+++ package/common/db.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	model "graphql_chat/package/model"
+	"log"
 	"os"
+	"time"
 )
 
 func InitPostgres() (*gorm.DB, error) {
@@ -14,9 +17,22 @@ func InitPostgres() (*gorm.DB, error) {
 
 	dsn := fmt.Sprintf("host=localhost user=postgres password=%s dbname=postgres port=5432 sslmode=disable", password)
 
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: false,         // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      false,         // Don't include params in the SQL log
+			Colorful:                  true,          // Disable color
+		},
+	)
+
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN: dsn,
-	}), &gorm.Config{})
+	}), &gorm.Config{
+		TranslateError: true,
+		Logger:         newLogger})
 
 	if err != nil {
 		return nil, err
